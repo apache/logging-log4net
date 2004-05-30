3a6db75268f4250317eaa9f4fc6bcc5df4026220
Nicko Cadell <nicko@apache.org>
2004-05-30 11:10:31 +0000

    Renamed loggingEvent.GetExceptionStrRep to GetExceptionString

--- src/Layout/ExceptionLayout.cs
+++ src/Layout/ExceptionLayout.cs
@@ -90,7 +90,7 @@ namespace log4net.Layout
 				throw new ArgumentNullException("loggingEvent");
 			}
 
-			writer.Write(loggingEvent.GetExceptionStrRep());
+			writer.Write(loggingEvent.GetExceptionString());
 		}
 
 		#endregion
