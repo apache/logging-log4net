869b1c882356c89c40de4a0f1350b9f63d82e130
Gert Driesen <drieseng@apache.org>
2004-07-24 14:54:42 +0000

    do not include this class in log4net Core as it contains win32 specific code

--- src/Appender/ColoredConsoleAppender.cs
+++ src/Appender/ColoredConsoleAppender.cs
@@ -24,6 +24,9 @@
 #if !MONO 
 // SSCLI 1.0 has no support for Win32 Console API's
 #if !SSCLI
+// We don't want framework or platform specific code in the Core version of
+// log4net
+#if !CORE
 
 using System;
 using System.Globalization;
@@ -425,6 +428,7 @@ namespace log4net.Appender
 	}
 }
 
+#endif // !CORE
 #endif // !SSCLI
 #endif // !MONO
 #endif // !NETCF
