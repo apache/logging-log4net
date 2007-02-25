74969bf22f63c1d59d09a952615e6228df4c203f
Nicko Cadell <nicko@apache.org>
2007-02-25 18:39:18 +0000

    added missing using directives.

--- examples/net/1.0/Repository/SimpleModule/cs/src/AssemblyInfo.cs
+++ examples/net/1.0/Repository/SimpleModule/cs/src/AssemblyInfo.cs
@@ -17,6 +17,9 @@
 //
 #endregion
 
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
