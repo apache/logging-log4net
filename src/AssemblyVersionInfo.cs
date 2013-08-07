25f8d5a2543c952f3f2bbeb1fbabb93ace4b3e6f
Dominik Psenner <dpsenner@apache.org>
2013-08-07 13:47:44 +0000

    Bump copyright year

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -42,5 +42,5 @@
 //
 
 [assembly: System.Reflection.AssemblyCompany("The Apache Software Foundation")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright 2004-2011 The Apache Software Foundation.")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright 2004-2013 The Apache Software Foundation.")]
 [assembly: System.Reflection.AssemblyTrademark("Apache and Apache log4net are trademarks of The Apache Software Foundation")]
