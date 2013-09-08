758890749170f52792fc75788a481c0727901d62
Stefan Bodewig <bodewig@apache.org>
2013-09-08 14:35:22 +0000

    merge fix for LOG4NET-390 from trunk

--- src/Appender/AppenderCollection.cs
+++ src/Appender/AppenderCollection.cs
@@ -883,6 +883,16 @@ namespace log4net.Appender
 				throw new NotSupportedException("This is a Read Only Collection and can not be modified");
 			}
 
+			public override IAppender[] ToArray()
+			{
+				return m_collection.ToArray();
+			}
+
+			public override void TrimToSize()
+			{
+				throw new NotSupportedException("This is a Read Only Collection and can not be modified");
+			}
+
 			#endregion
 		}
 
