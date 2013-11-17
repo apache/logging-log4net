1874be5bbe7166a70b930e2df6887d84c86dd4d8
Stefan Bodewig <bodewig@apache.org>
2013-11-17 09:40:47 +0000

    merge fix for LOG4NET-376 from 1.2.x branch

--- src/log4net/DateFormatter/AbsoluteTimeDateFormatter.cs
+++ src/log4net/DateFormatter/AbsoluteTimeDateFormatter.cs
@@ -101,6 +101,8 @@ namespace log4net.DateFormatter
 		/// </remarks>
 		virtual public void FormatDate(DateTime dateToFormat, TextWriter writer)
 		{
+                    lock (s_lastTimeStrings)
+		    {
 			// Calculate the current time precise only to the second
 			long currentTimeToTheSecond = (dateToFormat.Ticks - (dateToFormat.Ticks % TimeSpan.TicksPerSecond));
 
@@ -158,6 +160,7 @@ namespace log4net.DateFormatter
 				writer.Write('0');
 			}
 			writer.Write(millis);
+                    }
 		}
 
 		#endregion Implementation of IDateFormatter
