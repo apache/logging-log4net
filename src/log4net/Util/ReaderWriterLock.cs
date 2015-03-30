dbe0f89a38f6fca026d8d78ab59f23009cde8443
Stefan Bodewig <bodewig@apache.org>
2015-03-30 09:03:45 +0000

    LOG4NET-443 prevent orphaned locks with ReaderWriterLockSlim in the face of aborted threads

--- src/log4net/Util/ReaderWriterLock.cs
+++ src/log4net/Util/ReaderWriterLock.cs
@@ -88,7 +88,12 @@ namespace log4net.Util
 		{
 #if HAS_READERWRITERLOCK
 #if HAS_READERWRITERLOCKSLIM
+                    // prevent ThreadAbort while updating state, see https://issues.apache.org/jira/browse/LOG4NET-443
+                    try { } 
+                    finally
+                    {
 			m_lock.EnterReadLock();
+                    }
 #else
 			m_lock.AcquireReaderLock(-1);
 #endif
@@ -132,7 +137,12 @@ namespace log4net.Util
 		{
 #if HAS_READERWRITERLOCK
 #if HAS_READERWRITERLOCKSLIM
+                    // prevent ThreadAbort while updating state, see https://issues.apache.org/jira/browse/LOG4NET-443
+                    try { } 
+                    finally
+                    {
 			m_lock.EnterWriteLock();
+                    }
 #else
 			m_lock.AcquireWriterLock(-1);
 #endif
