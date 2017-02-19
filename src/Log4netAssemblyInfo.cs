c0b95f947307f2e99c4ae711a345e6f568348c6e
Stefan Bodewig <bodewig@apache.org>
2017-02-19 05:57:16 +0000

    add 'TargetFramework' .NET Core

--- src/Log4netAssemblyInfo.cs
+++ src/Log4netAssemblyInfo.cs
@@ -59,6 +59,8 @@ namespace log4net {
         public const string TargetFramework = "Mono";
 #elif SSCLI
         public const string TargetFramework = "Shared Source CLI";
+#elif NETSTANDARD1_3
+        public const string TargetFramework = ".NET Core";
 #else
         public const string TargetFramework = "Unknown";
 #endif
