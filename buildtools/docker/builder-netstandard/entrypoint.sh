7236dc8c652d764105b5e1d2c4bdf9865f619c0a
Dominik Psenner <dpsenner@apache.org>
2017-07-08 18:54:20 +0200

    Jenkins: also create a group with the given group id

--- buildtools/docker/builder-netstandard/entrypoint.sh
+++ buildtools/docker/builder-netstandard/entrypoint.sh
@@ -2,4 +2,5 @@
 echo "Setting up user '$USER' that maps to uid $(id -u)"
 
 export HOME=`pwd`
+groupadd --gid `id -g` $USER
 useradd --home-dir "$HOME" --shell /bin/bash --uid `id -u` --gid `id -g` --groups `id -g` -p -M "$USER"
