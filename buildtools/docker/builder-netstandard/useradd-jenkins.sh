f08520df12487df540e3dbe681f2358458b694aa
Dominik Psenner <dpsenner@apache.org>
2017-07-08 21:31:28 +0200

    builder-netstandard: add jenkins:jenkins-slave

--- buildtools/docker/builder-netstandard/useradd-jenkins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-L_USER="jenkins"
-L_HOME="/home/jenkins"
-L_UID=10025
-L_GID=12040
-groupadd --force --gid $L_GID $USER
-useradd --home-dir "$HOME" --shell /bin/bash --uid $L_UID --gid $L_GID --groups $L_GID -p -M $L_USER
