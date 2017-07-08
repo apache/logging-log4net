f62be04ea6d5fe652a8160e6898956d49919a74e
Dominik Psenner <dpsenner@apache.org>
2017-07-08 21:00:03 +0200

    builder-netstandard: force add of group

--- buildtools/docker/builder-netstandard/useradd-jenkins.sh
+++ buildtools/docker/builder-netstandard/useradd-jenkins.sh
@@ -3,5 +3,5 @@ L_USER="jenkins"
 L_HOME="/home/jenkins"
 L_UID=10025
 L_GID=12040
-groupadd --gid $L_GID $USER
+groupadd --force --gid $L_GID $USER
 useradd --home-dir "$HOME" --shell /bin/bash --uid $L_UID --gid $L_GID --groups $L_GID -p -M $L_USER
