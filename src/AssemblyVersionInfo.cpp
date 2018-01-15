978db2796944ea27ea71ac37ce8d376bef94c0bf
Stefan Bodewig <stefan.bodewig@innoq.com>
2018-01-15 09:18:54 +0100

    happy new year

--- src/AssemblyVersionInfo.cpp
+++ src/AssemblyVersionInfo.cpp
@@ -1,9 +1,9 @@
 //
-// Licensed to the Apache Software Foundation (ASF) under one or more 
+// Licensed to the Apache Software Foundation (ASF) under one or more
 // contributor license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright ownership. 
+// this work for additional information regarding copyright ownership.
 // The ASF licenses this file to you under the Apache License, Version 2.0
-// (the "License"); you may not use this file except in compliance with 
+// (the "License"); you may not use this file except in compliance with
 // the License. You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
@@ -24,11 +24,11 @@ using namespace System::Runtime::CompilerServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the value or you can default the Revision and Build Numbers 
+// You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
 [assembly: AssemblyVersionAttribute("2.0.9.0")];
@@ -44,5 +44,5 @@ using namespace System::Runtime::CompilerServices;
 //
 
 [assembly: AssemblyCompany("The Apache Software Foundation")];
-[assembly: AssemblyCopyright("Copyright 2004-2017 The Apache Software Foundation.")];
+[assembly: AssemblyCopyright("Copyright 2004-2018 The Apache Software Foundation.")];
 [assembly: AssemblyTrademark("Apache and Apache log4net are trademarks of The Apache Software Foundation")];
