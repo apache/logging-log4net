7b3669bba39fb739fe593459e236327c8976e8e5
tt1366 <yim.tim@bcthk.com>
2018-12-13 16:24:28 +0800

    Set the setter and getter to support net 2.0 runtime.

--- src/Filter/ExceptionTypeFilter.cs
+++ src/Filter/ExceptionTypeFilter.cs
@@ -7,24 +7,46 @@ namespace log4net.Filter
 {
     public class ExceptionTypeFilter : FilterSkeleton
     {
-        public ExceptionTypeFilter()
-        {
-            this.AcceptOnMatch = true;
-        }
+
+        private bool m_acceptOnMatch = true;
+        private string m_exceptionTypeName;
 
         /// <summary>
         /// Set the exception type name to filter
         /// </summary>
-        public string ExceptionTypeName { get; set; }
+        public string ExceptionTypeName
+        {
+            get
+            {
+                return this.m_exceptionTypeName;
+            }
+            set
+            {
+                this.m_exceptionTypeName = value;
+            }
+        }
 
         /// <summary>
         /// <see cref="FilterDecision.Accept"/> when matching <see cref="ExceptionTypeName"/>
         /// Default value is true.
         /// </summary>
-        public bool AcceptOnMatch { get; set; }
+        public bool AcceptOnMatch
+        {
+            get
+            {
+                return this.m_acceptOnMatch;
+            }
+            set
+            {
+                this.m_acceptOnMatch = value;
+            }
+        }
 
         public override FilterDecision Decide(LoggingEvent loggingEvent)
         {
+            if (String.IsNullOrEmpty(this.ExceptionTypeName))
+                return FilterDecision.Neutral;
+
             var myExceptionType = Type.GetType(this.ExceptionTypeName, false);
 
             if (this.ExceptionTypeName != null
