65437791f12d73afd7f09ce49981eeb35440c5f3
Farhan Nasim <f.nasim01@gmail.com>
2019-06-02 16:44:30 +0600

    Override CreateLogger in RollingFileAppenderTrailingTest.

--- tests/src/Appender/RollingFileAppenderTrailingTest.cs
+++ tests/src/Appender/RollingFileAppenderTrailingTest.cs
@@ -37,6 +37,39 @@ namespace log4net.Tests.Appender
 			Assert.AreEqual("00:05:00", appender.TrailPeriod);
 		}
 
+		protected override ILogger CreateLogger(string filename, FileAppender.LockingModelBase lockModel, IErrorHandler handler, int maxFileSize, int maxSizeRollBackups, RollingFileAppender.RollingLockStrategyKind rollingLockStrategy)
+		{
+			Repository.Hierarchy.Hierarchy h = (Repository.Hierarchy.Hierarchy)LogManager.CreateRepository("TestRepository");
+
+			RollingFileAppenderTrailing appender = new RollingFileAppenderTrailing();
+			appender.File = filename;
+			appender.AppendToFile = false;
+			appender.CountDirection = 0;
+			appender.RollingStyle = RollingFileAppender.RollingMode.Size;
+			appender.MaxFileSize = maxFileSize;
+			appender.Encoding = Encoding.ASCII;
+			appender.ErrorHandler = handler;
+			appender.MaxSizeRollBackups = maxSizeRollBackups;
+			appender.RollingLockStrategy = rollingLockStrategy;
+			if (lockModel != null)
+			{
+				appender.LockingModel = lockModel;
+			}
+
+			PatternLayout layout = new PatternLayout();
+			layout.ConversionPattern = "%m%n";
+			layout.ActivateOptions();
+
+			appender.Layout = layout;
+			appender.ActivateOptions();
+
+			h.Root.AddAppender(appender);
+			h.Configured = true;
+
+			ILogger log = h.GetLogger("Logger");
+			return log;
+		}
+
 		private ILogger CreateLogger(string filename, long maxFileSize, IErrorHandler handler, int maxSizeRollBackups,
 			RollingFileAppender.RollingLockStrategyKind rollingLockStrategy, FileAppender.LockingModelBase lockModel)
 		{
