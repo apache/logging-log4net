730d7bf695f4a02a9b7e1ffc10f5e2928df5bcc6
Davyd McColl <davydm@gmail.com>
2022-07-22 14:36:54 +0200

    :white_check_mark: try to repro LOG4NET-611, but no go

---
+++ src/integration-testing/log4net-611-lib/DerivedAppender.cs
@@ -0,0 +1,8 @@
+﻿using log4net.Appender;
+using log4net.Layout;
+
+namespace log4net_611_lib;
+
+public class DerivedAppender: RollingFileAppender
+{
+}
---
+++ src/integration-testing/log4net-611-main/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net_611_lib;
+
+var appPath = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
+var appFolder = Path.GetDirectoryName(appPath);
+// force loading the assembly, otherwise the appender type isn't found later
+Assembly.LoadFile(Path.Combine(appFolder, "log4net-611-lib.dll"));
+
+var configFile = Path.Combine(appFolder, "log4net.config");
+if (!File.Exists(configFile))
+{
+    throw new InvalidOperationException($"log4net.config not found at {configFile}");
+}
+
+if (Directory.Exists("Logs"))
+{
+    Console.WriteLine("Clearing out old logs...");
+    foreach (var file in Directory.EnumerateFiles("Logs"))
+    {
+        File.Delete(file);
+    }
+}
+
+var info = new FileInfo(configFile);
+var logRepo = LogManager.GetRepository(Assembly.GetExecutingAssembly());
+XmlConfigurator.ConfigureAndWatch(
+    logRepo,
+    info
+);
+
+var logger = LogManager.GetLogger(typeof(Program));
+
+Console.WriteLine("logging...");
+for (var i = 0; i < 10; i++)
+{
+    logger.Info($"test log {i}");
+}
+
+
+foreach (var file in Directory.EnumerateFiles("Logs"))
+{
+    Console.WriteLine($"log file: {file}");
+    Console.WriteLine(File.ReadAllText(file));
+}
