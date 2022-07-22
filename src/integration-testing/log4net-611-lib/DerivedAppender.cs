b04703397e3bdc246e9d5746f2f3ecee2012e625
Davyd McColl <davydm@gmail.com>
2022-07-22 14:50:21 +0200

    :sparkles: make it obvious that the derived appender is being used

--- src/integration-testing/log4net-611-lib/DerivedAppender.cs
+++ src/integration-testing/log4net-611-lib/DerivedAppender.cs
@@ -1,8 +1,14 @@
 ﻿using log4net.Appender;
+using log4net.Core;
 using log4net.Layout;
 
 namespace log4net_611_lib;
 
 public class DerivedAppender: RollingFileAppender
 {
+    protected override void Append(LoggingEvent loggingEvent)
+    {
+        loggingEvent.Properties["appender-class-name"] = nameof(DerivedAppender);
+        base.Append(loggingEvent);
+    }
 }
