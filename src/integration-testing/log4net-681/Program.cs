692eee8a6ca99bf6b1a8cd6b44d48614fe0e7b64
Davyd McColl <davydm@gmail.com>
2022-07-22 16:03:37 +0200

    :alembic: try to repro LOG4NET-681, without success

--- src/integration-testing/log4net-681/Program.cs
+++ src/integration-testing/log4net-681/Program.cs
@@ -1,18 +1,14 @@
 ﻿using System.Reflection;
 using log4net;
 using log4net.Config;
-using log4net_611_lib;
 
 var appPath = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
 var appFolder = Path.GetDirectoryName(appPath);
-// force loading the assembly, otherwise the appender type isn't found later
-Assembly.LoadFile("log4net-611-lib.dll");
+
 if (appFolder is null)
 {
     throw new InvalidOperationException("Can't find myself");
 }
-
-Assembly.LoadFile(Path.Combine(appFolder, "log4net-611-lib.dll"));
 
 var configFile = Path.Combine(appFolder, "log4net.config");
 if (!File.Exists(configFile))
@@ -39,14 +35,28 @@ XmlConfigurator.ConfigureAndWatch(
 var logger = LogManager.GetLogger(typeof(Program));
 
 Console.WriteLine("logging...");
-for (var i = 0; i < 10; i++)
+var threads = new List<Thread>();
+for (var i = 0; i < 128; i++)
 {
-    logger.Info($"test log {i}");
+    var thread = new Thread(LogABit);
+    thread.Start();
+    threads.Add(thread);
 }
 
+foreach (var t in threads)
+{
+    t.Join();
+}
 
 foreach (var file in Directory.EnumerateFiles("Logs"))
 {
-    Console.WriteLine($"log file: {file}");
-    Console.WriteLine(File.ReadAllText(file));
+    Console.WriteLine($"found log file: {file}");
+}
+
+void LogABit()
+{
+    for (var i = 0; i < 100; i++)
+    {
+        logger.Info($"test log {i}");
+    }
 }
