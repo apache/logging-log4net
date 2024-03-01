66891fdde50c8ad16814ae7cd4867cdc9db1e687
Davyd McColl <davydm@gmail.com>
2024-03-01 14:54:55 +0200

    :bug: should delete old site before asking mvn to make a new one

--- local-tasks/build-site.js
+++ local-tasks/build-site.js
@@ -5,9 +5,9 @@
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
 // with the License.  You may obtain a copy of the License at
-// 
+//
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,32 +16,33 @@
 // under the License.
 
 const
-    gulp = requireModule("gulp"),
-    spawn = requireModule("spawn"),
-    env = requireModule("env"),
-    os = require("os"),
-    which = require("which");
+  gulp = requireModule("gulp"),
+  spawn = requireModule("spawn"),
+  env = requireModule("env"),
+  os = require("os"),
+  which = require("which");
 
 gulp.task("build-site", async () => {
-    let maven;
-    try {
-        maven = await which("mvn");
-    } catch (e) {
-        let extra;
-        switch (os.platform()) {
-            case "win32":
-                extra = "You may install maven via chocolatey (https://chocolatey.org)";
-                break;
-            case "darwin":
-                extra = "You may install maven via homebrew";
-                break;
-            default:
-                extra = "You should install maven with your package manager";
-                break;
-        }
-        throw new Error(`Unable to find mvn in your path. ${extra}`);
+  const { rm } = require("yafs");
+  let maven;
+  try {
+    maven = await which("mvn");
+  } catch (e) {
+    let extra;
+    switch (os.platform()) {
+      case "win32":
+        extra = "You may install maven via chocolatey (https://chocolatey.org)";
+        break;
+      case "darwin":
+        extra = "You may install maven via homebrew";
+        break;
+      default:
+        extra = "You should install maven with your package manager";
+        break;
     }
-
-    return spawn("mvn", [ "site" ]);
+    throw new Error(`Unable to find mvn in your path. ${ extra }`);
+  }
+  await rm("target");
+  return spawn("mvn", [ "site" ]);
 });
 
