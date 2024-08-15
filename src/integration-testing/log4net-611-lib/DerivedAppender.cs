5b0116bcfe30deeede975cb6289a701c636d3044
Jan Friedrich <freeandnil@apache.org>
2024-08-15 16:51:56 +0200

    #168 wrap encoding in ColoredConsoleAppender for skipping preamble

--- src/integration-testing/log4net-611-lib/DerivedAppender.cs
+++ src/integration-testing/log4net-611-lib/DerivedAppender.cs
@@ -1,14 +1,13 @@
 ﻿using log4net.Appender;
 using log4net.Core;
-using log4net.Layout;
 
 namespace log4net_611_lib;
 
-public class DerivedAppender: RollingFileAppender
+public class DerivedAppender : RollingFileAppender
 {
-    protected override void Append(LoggingEvent loggingEvent)
-    {
-        loggingEvent.Properties["appender-class-name"] = nameof(DerivedAppender);
-        base.Append(loggingEvent);
-    }
+  protected override void Append(LoggingEvent loggingEvent)
+  {
+    loggingEvent.Properties["appender-class-name"] = nameof(DerivedAppender);
+    base.Append(loggingEvent);
+  }
 }
--- src/integration-testing/log4net-672/Program.cs
+++ src/integration-testing/log4net-672/Program.cs
@@ -14,43 +14,41 @@ var appPath = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
 var appFolder = Path.GetDirectoryName(appPath);
 if (appFolder is null)
 {
-    throw new InvalidOperationException(
-        $"Can't determine app folder for {appPath}"
-    );
+  throw new InvalidOperationException($"Can't determine app folder for {appPath}");
 }
 
 var logFolder = Path.Combine(appFolder, "Logs");
 if (Directory.Exists(logFolder))
 {
-    Directory.Delete(logFolder, recursive: true);
+  Directory.Delete(logFolder, recursive: true);
 }
 
 var configFile = Path.Combine(appFolder, "log4net.config");
 if (!File.Exists(configFile))
 {
-    throw new InvalidOperationException($"log4net.config not found at {configFile}");
+  throw new InvalidOperationException($"log4net.config not found at {configFile}");
 }
 
 var logCount = 10;
 var identifiers = new List<Guid>();
 for (var i = 0; i < 10; i++)
 {
-    var identifier = Guid.NewGuid();
-    identifiers.Add(identifier);
-    var logged = LogWith(identifier, logCount);
-    if (logged != logCount)
-    {
-        Die($"Missing logs immediately for '{identifier}' - found {logged}/{logCount}", MISSING_LOGS);
-    }
+  var identifier = Guid.NewGuid();
+  identifiers.Add(identifier);
+  var logged = LogWith(identifier, logCount);
+  if (logged != logCount)
+  {
+    Die($"Missing logs immediately for '{identifier}' - found {logged}/{logCount}", MISSING_LOGS);
+  }
 }
 
 foreach (var identifier in identifiers)
 {
-    var logged = CountIdentifierInLogs(identifier);
-    if (logged != logCount)
-    {
-        Die($"Logs have been overwritten for '{identifier}' - found {logged}/{logCount}", OVERWRITTEN_LOGS);
-    }
+  var logged = CountIdentifierInLogs(identifier);
+  if (logged != logCount)
+  {
+    Die($"Logs have been overwritten for '{identifier}' - found {logged}/{logCount}", OVERWRITTEN_LOGS);
+  }
 }
 
 Console.WriteLine("All good: LOG4NET-672 is resolved");
@@ -58,34 +56,34 @@ return NO_ERROR;
 
 void Die(string message, int exitCode)
 {
-    Console.Error.WriteLine(message);
-    Environment.Exit(exitCode);
+  Console.Error.WriteLine(message);
+  Environment.Exit(exitCode);
 }
 
 int CountIdentifierInLogs(Guid id)
 {
-    return Directory.EnumerateFiles("Logs").Select(
-        filePath => CountIdentifierInFile(id, filePath)
-    ).Sum();
+  return Directory.EnumerateFiles("Logs").Select(
+      filePath => CountIdentifierInFile(id, filePath)
+  ).Sum();
 }
 
 int CountIdentifierInFile(Guid id, string filePath)
 {
-    var contents = File.ReadAllLines(filePath);
-    return contents.Count(line => line.Contains(id.ToString()));
+  var contents = File.ReadAllLines(filePath);
+  return contents.Count(line => line.Contains(id.ToString()));
 }
 
 int LogWith(Guid identifier, int howManyLogs)
 {
-    var info = new FileInfo(configFile);
-    XmlConfigurator.Configure(info);
-    var logger = LogManager.GetLogger("main");
+  var info = new FileInfo(configFile);
+  XmlConfigurator.Configure(info);
+  var logger = LogManager.GetLogger("main");
 
-    for (var i = 0; i < howManyLogs; i++)
-    {
-        logger.Info($"test log {i} [{identifier}]");
-    }
+  for (var i = 0; i < howManyLogs; i++)
+  {
+    logger.Info($"test log {i} [{identifier}]");
+  }
 
-    LogManager.Flush(int.MaxValue);
-    return CountIdentifierInLogs(identifier);
+  LogManager.Flush(int.MaxValue);
+  return CountIdentifierInLogs(identifier);
 }
