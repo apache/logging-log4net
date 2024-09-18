8239645e1b97794f92a7421a8915035d2b56737d
Jan Friedrich <freeandnil@apache.org>
2024-09-18 23:30:48 +0200

    extended preview script for signing and tag creation

--- scripts/build-preview.ps1
+++ scripts/build-preview.ps1
@@ -1,1 +1,12 @@
-dotnet build -c Release '-p:GeneratePackages=true;PackageVersion=3.0.1-preview.2' $PSScriptRoot/../src/log4net/log4net.csproj
+$Version = '3.0.1'
+$Preview = '2'
+'building ...'
+dotnet build -c Release "-p:GeneratePackages=true;PackageVersion=$Version-preview.$Preview" $PSScriptRoot/../src/log4net/log4net.csproj
+'signing ...'
+gpg --armor --output $PSScriptRoot\..\build\artifacts\log4net.$Version-preview.$Preview.nupkg.asc --detach-sig $PSScriptRoot\..\build\artifacts\log4net.$Version-preview.$Preview.nupkg
+'create tag?'
+pause
+'creating tag ...'
+git tag "rc/$Version-preview.$Preview"
+'pushing tag ...'
+git push --tags
